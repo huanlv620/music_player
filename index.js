565cee5de327f15c06be4609583256e7fb34b5ae
huanlv <huanlv620@gmail.com>
2023-02-26 17:28:53 +0700

    setup music player

---
+++ index.js
@@ -0,0 +1,54 @@
+/**
+ * 1. render songs
+ * 2. scroll top
+ * 3. play / pause / seek
+ *
+ */
+
+
+const app = {
+  songs: [
+    {
+      name: "Intro",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song1.mp3",
+      image: "assets/img/song1.jpg",
+    },
+    {
+      name: "Cơn mưa xa dần",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song2.mp3",
+      image: "assets/img/song2.jpg",
+    },
+
+    {
+      name: "Nắng ấm ngang qua",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song3.mp3",
+      image: "assets/img/song3.jpg",
+    },
+    {
+      name: "Muộn rồi mà sao còn",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song4.mp3",
+      image: "assets/img/song4.jpg",
+    },
+    {
+      name: "Hãy trao cho anh",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song5.mp3",
+      image: "assets/img/song5.jpg",
+    },
+    {
+      name: "Em của ngày hôm qua",
+      singer: "Sơn Tùng MTP",
+      path: "assets/music/song6.mp3",
+      image: "assets/img/song6.jpg",
+    },
+  ],
+
+  start: function () {
+  },
+};
+
+app.start();
